aa4b48af06a1c114eef49316546e093c014479a6
levelarde <leonardo.velarde.ldvc@gmail.com>
2017-03-09 18:03:31 -0400

    added vector multiplication functions

--- Vector.rb
+++ Vector.rb
@@ -20,4 +20,30 @@ class Vector
 		@y -= vectorParaRestar.y
 		@z -= vectorParaRestar.z
 	end
+
+	def  modulo
+		Math.sqrt(@x**2 + @y**2 + @z**2)
+	end
+
+	def multiplicarVectorialmentePor vectorParaMultiplicar
+		@x = (@y * vectorParaMultiplicar.z) - (@z * vectorParaMultiplicar.y)
+		@y = (@z * vectorParaMultiplicar.x) - (@x * vectorParaMultiplicar.z)
+		@z = (@x * vectorParaMultiplicar.y) - (@y * vectorParaMultiplicar.x)
+	end
+
+	def multiplicarEscalarmentePor vectorParaMultiplicar
+		i = @x + vectorParaMultiplicar.x
+		j = @y + vectorParaMultiplicar.y
+		k = @z + vectorParaMultiplicar.z
+		return (i + j + k).to_f
+	end
+
+	def self.multiplicacionVectorial (vec1, vec2)
+		i = (vec1.y * vec2.z) - (vec1.z * vec2.y)
+		j = (vec1.z * vec2.x) - (vec1.x * vec2.z)
+		k = (vec1.x * vec2.y) - (vec1.y * vec2.x)
+		return Vector.new(i, j, k)
+	end
+
+
 end
